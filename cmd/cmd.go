6a1ee7f4f2407d73a39d064785f2c6db6b5dc33b
Júlia Biró <julia.biro@gmail.com>
2021-01-17 16:41:54 +0100

    test

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,9 +1,6 @@
 package cmd
 
 func ParseInput(args []string) ([]string, error) {
-	var ret []string
-	for _, c := range args {
-		ret = append(ret, c)
-	}
-	return ret, nil
+	// this will become more complex later
+	return args, nil
 }
