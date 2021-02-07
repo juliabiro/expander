4885e11c3f9d3238253784880dca594125367cef
Julia Biro <julia.biro@gmail.com>
2021-02-07 17:49:28 +0100

    abbreviator tests fixd

--- pkg/abbreviator/abbreviatior_test.go
+++ pkg/abbreviator/abbreviatior_test.go
@@ -32,43 +32,26 @@ func TestAbbreviate(t *testing.T) {
 func TestAbbreviateExpressionsNoRepeats(t *testing.T) {
 	input := []string{"apple-pear", "apple0pear"}
 	m := []map[string]string{{"-": ""}, {"0": ""}}
-	output := AbbreviateExpressions(input, m)
+	ed := utils.ExpanderData{m, map[string]string{}, map[string]string{}}
+	err := AbbreviateExpressions(input, &ed)
 
-	if output != nil {
+	if err == nil {
 		t.Fatalf("Expected to abort on repeated abbreviation, but it went forward.")
 	}
 
 }
 
-// func TestParseConfigFile(t *testing.T) {
-// 	abbreviations := make([]utils.StringPair, 0)
+func TestParseDataFile(t *testing.T) {
+	data := ParseDataFile("")
+	if data != nil {
+		t.Fatalf("Shouldn't read anything from a file with no name")
+	}
 
-// 	ParseConfigFile("", &abbreviations)
-// 	if len(abbreviations) != 0 {
-// 		t.Fatalf("Shouldn't read anything from a file with no name")
-// 	}
+	e1 := utils.ReadDataFromFile("../../example_conf.json")
+	e2 := ParseDataFile("../../example_conf.json")
 
-// 	ParseConfigFile("../../example_mapping", &abbreviations)
+	if !e1.IsIdentical(e2) {
+		t.Fatalf("ParseDataFile shou;dnt do anything extra upon the utils version")
+	}
 
-// 	expected := []utils.StringPair{
-// 		utils.StringPair{"apple", "a"},
-// 		utils.StringPair{"pear", "p"},
-// 		utils.StringPair{"domain1.com", "d1"},
-// 		utils.StringPair{"domain2.com", "d2"},
-// 		utils.StringPair{"production", "p"},
-// 		utils.StringPair{"staging", "s"},
-// 		utils.StringPair{"-0", ""},
-// 		utils.StringPair{"-", ""},
-// 	}
-
-// 	if len(abbreviations) != len(expected) {
-// 		t.Fatalf("Parsing example_mapping, I didn't get the expected length of data")
-// 		return
-// 	}
-
-// 	for i, _ := range abbreviations {
-// 		if abbreviations[i] != expected[i] {
-// 			t.Fatalf("Mismatch while reading in the example mapping. Ezpected %s but got %s", expected[i], abbreviations[i])
-// 		}
-// 	}
-// }
+}
